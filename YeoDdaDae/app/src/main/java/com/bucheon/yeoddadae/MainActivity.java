82e95d498e5c37555f9d94406d84384a9165632b
rlaangks1 <rlaangks1@naver.com>
2024-03-10 06:16:03 +0900

    항상듣는stt 완료, SttActivity 삭제 및 바로 명령어 듣는 기능으로 변경

--- YeoDdaDae/app/src/main/java/com/bucheon/yeoddadae/MainActivity.java
+++ YeoDdaDae/app/src/main/java/com/bucheon/yeoddadae/MainActivity.java
@@ -1,53 +1,79 @@
 package com.bucheon.yeoddadae;
 
-import static android.content.ContentValues.TAG;
-
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.os.Handler;
-import android.speech.RecognitionListener;
-import android.speech.RecognizerIntent;
-import android.speech.SpeechRecognizer;
+import android.os.IBinder;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.util.List;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SttService.SttCallback {
 
     String loginId = null;
     boolean isAdmin = false;
-    Stt stt = new Stt(this);
-    
-    //Intent 리퀘스트 코드들
+
+    private Intent serviceIntent;
+
+    private SttService.SttCallback sttCallback = new SttService.SttCallback() {
+        @Override
+        public void onMainCommandReceived(String mainCommand) {
+            MainActivity.this.onMainCommandReceived(mainCommand);
+        }
+    };
+
+    private SttService sttService;
+
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            SttService.SttBinder binder = (SttService.SttBinder) service;
+            sttService = binder.getService();
+            sttService.setSttCallback(sttCallback);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            // 필요한 경우 연결이 끊겼을 때 처리
+        }
+    };
+
     final int loginIntentRequestCode = 1;
-    final int sttIntentRequestCode = 2;
+
+    Button toLoginBtn;
+    TextView nowIdTxt;
+    TextView isAdminTxt;
+    TextView toSttBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button toLoginBtn = (Button) findViewById(R.id.toLoginBtn);
-        TextView nowIdTxt = (TextView) findViewById(R.id.nowId);
-        TextView isAdminTxt = (TextView) findViewById(R.id.isAdmin);
-        TextView toSttBtn = (TextView) findViewById(R.id.toSttBtn);
+        serviceIntent = new Intent(this, SttService.class);
+        startService(serviceIntent);
+        bindService(serviceIntent, serviceConnection, Context.BIND_AUTO_CREATE);
+
+        toLoginBtn = findViewById(R.id.toLoginBtn);
+        nowIdTxt = findViewById(R.id.nowId);
+        isAdminTxt = findViewById(R.id.isAdmin);
+        toSttBtn = findViewById(R.id.toSttBtn);
 
         nowIdTxt.setText(loginId);
         isAdminTxt.setText(Boolean.toString(isAdmin));
 
         toLoginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick (View v) {
+            public void onClick(View v) {
                 if (loginId == null) {
                     Intent loginIntent = new Intent(getApplicationContext(), LoginActivity.class);
                     startActivityForResult(loginIntent, loginIntentRequestCode);
-                }
-                else {
+                } else {
                     loginId = null;
                     isAdmin = false;
 
@@ -62,21 +88,22 @@ public class MainActivity extends AppCompatActivity {
         toSttBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent sttIntent = new Intent(getApplicationContext(), SttActivity.class);
-                startActivityForResult(sttIntent, sttIntentRequestCode);
+                if (sttService != null) {
+                    sttService.startListeningForMainCommand();
+                }
             }
         });
+    }
 
-        stt.startListeningForWakeUpWord();
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sttService.stopListening();
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
-        Button toLoginBtn = (Button) findViewById(R.id.toLoginBtn);
-        TextView nowIdTxt = (TextView) findViewById(R.id.nowId);
-        TextView isAdminTxt = (TextView) findViewById(R.id.isAdmin);
 
         if (requestCode == loginIntentRequestCode) {
             if (resultCode == RESULT_OK) {
@@ -89,10 +116,45 @@ public class MainActivity extends AppCompatActivity {
                 toLoginBtn.setText("로그아웃");
             }
         }
+        startService(serviceIntent);
+    }
+
+
+
+    @Override
+    public void onMainCommandReceived(String mainCommand) {
+        Log.d("TAG", "MainActivity에서 받은 명령: " + mainCommand);
+
+        if (mainCommand.contains("로그인")) {
+            if (loginId == null) {
+                Intent loginIntent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivityForResult(loginIntent, loginIntentRequestCode);
+            } else {
+                Log.d("TAG", "이미 로그인 상태임");
+            }
+        } else if (mainCommand.contains("로그아웃")) {
+            if (loginId != null) {
+                loginId = null;
+                isAdmin = false;
+
+                Button toLoginBtn = findViewById(R.id.toLoginBtn);
+                TextView nowIdTxt = findViewById(R.id.nowId);
+                TextView isAdminTxt = findViewById(R.id.isAdmin);
+
+                nowIdTxt.setText(loginId);
+                isAdminTxt.setText(Boolean.toString(isAdmin));
+
+                toLoginBtn.setText("로그인");
+            } else {
+                Log.d("TAG", "이미 로그아웃 상태임");
+            }
+        }
+        startService(serviceIntent);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        unbindService(serviceConnection);
     }
 }
---
+++ YeoDdaDae/app/src/main/java/com/bucheon/yeoddadae/SttService.java
@@ -0,0 +1,240 @@
+package com.bucheon.yeoddadae;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.util.Log;
+
+import java.util.List;
+
+public class SttService extends Service {
+
+    private static final String TAG = "SttService";
+    private SpeechRecognizer speechRecognizer;
+    private SttCallback sttCallback;
+    private boolean isListeningForWakeUpWord = false;
+
+    public class SttBinder extends Binder {
+        public SttService getService() {
+            return SttService.this;
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return new SttBinder();
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        listenForWakeUpWord();
+        return START_STICKY;
+    }
+
+    private void stopContinuousListening() {
+        if (speechRecognizer != null) {
+            speechRecognizer.destroy();
+        }
+    }
+
+    public void startListeningForMainCommand() {
+        if (isListeningForWakeUpWord) {
+            stopContinuousListening();
+            listenForMainCommand();
+        }
+    }
+
+    public void stopListening() {
+        stopContinuousListening();
+    }
+
+    private void listenForWakeUpWord() {
+        isListeningForWakeUpWord = true;
+
+        // 음성 인식을 위한 Intent 생성
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ko-KR"); // 언어를 한국어로 설정
+
+        // 듣기 시작
+        speechRecognizer.startListening(intent);
+
+        // RecognitionListener 설정
+        speechRecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle params) {
+                Log.d(TAG, "호출어듣기 : 준비완료");
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {Log.d(TAG, "호출어듣기 : 말하기시작");}
+
+            @Override
+            public void onResults(Bundle results) {
+                List<String> matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+
+                if (matches != null && matches.size() > 0) {
+                    String wakeUpWord = matches.get(0);
+
+                    Log.d(TAG, "호출어듣기 : 내용 : " + wakeUpWord);
+
+                    // 웨이크업 워드가 감지되었는지 확인
+                    if (wakeUpWord.contains("음성 명령")) {
+                        Log.d(TAG, "호출어듣기 : 호출어확인성공");
+                        isListeningForWakeUpWord = false;  // 플래그를 false로 설정
+                        listenForMainCommand();
+
+                    }
+                    else {
+                        Log.d(TAG, "호출어듣기 : 호출어가아님");
+                        // 계속해서 웨이크업 워드를 듣기 시작
+                        // 돌아가기를 처리하기 전에 플래그를 확인합니다.
+                        if (isListeningForWakeUpWord) {
+                            listenForWakeUpWord();
+                        }
+                    }
+                }
+                else {
+                    // 웨이크업 워드를 듣지 못한 경우에 대한 처리를 여기에 추가
+                    Log.d(TAG, "호출어듣기 : 웨이크업 워드를 듣지 못했습니다. 다시 시도합니다.");
+                    listenForWakeUpWord();
+                }
+            }
+
+            @Override
+            public void onError(int error) {
+                switch (error) {
+                    case SpeechRecognizer.ERROR_NO_MATCH:
+                        // 음성을 인식하지 못했을 때의 처리를 여기에 추가
+                        Log.d(TAG, "호출어듣기 : 음성을 인식하지 못했습니다");
+                        listenForWakeUpWord(); // 다시 웨이크업 워드를 듣기 시작
+                        break;
+                    case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
+                        // 음성 입력이 타임아웃 되었을 때의 처리를 여기에 추가
+                        Log.d(TAG, "호출어듣기 : 타임아웃");
+                        listenForWakeUpWord(); // 다시 웨이크업 워드를 듣기 시작
+                        break;
+                    // 다른 오류 코드에 대한 처리를 추가할 수 있습니다.
+                    default:
+                        // 그 외의 경우에 대한 처리
+                        break;
+                }
+            }
+
+            @Override
+            public void onRmsChanged(float rmsdB) {}
+            @Override
+            public void onBufferReceived(byte[] buffer) {}
+            @Override
+            public void onEndOfSpeech() {}
+            @Override
+            public void onPartialResults(Bundle partialResults) {}
+            @Override
+            public void onEvent(int eventType, Bundle params) {}
+        });
+    }
+
+    private void listenForMainCommand() {
+        // 음성 인식을 위한 Intent 생성
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ko-KR"); // 언어를 한국어로 설정
+
+        // 듣기 시작
+        speechRecognizer.startListening(intent);
+
+        // RecognitionListener 설정
+        speechRecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle params) {
+                Log.d(TAG, "명령어듣기 : 준비완료");
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+                Log.d(TAG, "명령어듣기 : 말하기시작");
+            }
+
+            @Override
+            public void onResults(Bundle results) {
+                List<String> matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+
+                if (matches != null && matches.size() > 0) {
+                    String mainCommand = matches.get(0);
+
+                    Log.d(TAG, "명령어듣기 : 내용 : " + mainCommand);
+
+                    // 콜백을 사용하여 mainCommand 전달
+                    if (sttCallback != null) {
+                        sttCallback.onMainCommandReceived(mainCommand);
+                    }
+                }
+
+                // 메인 명령을 처리한 후, 플래그에 따라 웨이크업 워드를 계속 듣을지 결정합니다.
+                if (isListeningForWakeUpWord) {
+                    listenForWakeUpWord();
+                }
+            }
+
+            @Override
+            public void onError(int error) {
+                switch (error) {
+                    case SpeechRecognizer.ERROR_NO_MATCH:
+                        // 음성을 인식하지 못했을 때의 처리를 여기에 추가
+                        Log.d(TAG, "명령어듣기 : 음성을 인식하지 못했습니다");
+                        if (isListeningForWakeUpWord) {
+                            listenForWakeUpWord();
+                        }
+                        break;
+                    case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
+                        // 음성 입력이 타임아웃 되었을 때의 처리를 여기에 추가
+                        Log.d(TAG, "명령어듣기 : 타임아웃");
+                        if (isListeningForWakeUpWord) {
+                            listenForWakeUpWord();
+                        }
+                        break;
+                    // 다른 오류 코드에 대한 처리를 추가할 수 있습니다.
+                    default:
+                        // 그 외의 경우에 대한 처리
+                        break;
+                }
+            }
+
+            @Override
+            public void onRmsChanged(float rmsdB) {}
+            @Override
+            public void onBufferReceived(byte[] buffer) {}
+            @Override
+            public void onEndOfSpeech() {}
+            @Override
+            public void onPartialResults(Bundle partialResults) {}
+            @Override
+            public void onEvent(int eventType, Bundle params) {}
+        });
+    }
+
+    public interface SttCallback {
+        void onMainCommandReceived(String mainCommand);
+    }
+
+    public void setSttCallback(SttCallback callback) {
+        sttCallback = callback;
+    }
+
+    @Override
+    public void onDestroy() {
+        stopContinuousListening();
+        super.onDestroy();
+    }
+}
