63b5397be67f220f8241e3dff4a8374a303f976a
rlaangks1 <rlaangks1@naver.com>
2024-04-12 12:50:50 +0900

    예약 화면/기능 구현 중 reverseGeocoding메소드 사용법 알아내는 중

--- YeoDdaDae/app/src/main/java/com/bucheon/yeoddadae/ReservationParkActivity.java
+++ YeoDdaDae/app/src/main/java/com/bucheon/yeoddadae/ReservationParkActivity.java
@@ -5,23 +5,41 @@ import static android.content.ContentValues.TAG;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.skt.Tmap.TMapData;
+import com.skt.Tmap.address_info.TMapAddressInfo;
 
 import java.util.HashMap;
 
 public class ReservationParkActivity extends AppCompatActivity {
     String reservationFirestoreDocumentId;
+
+    TextView reservationParkAddressContentTxt;
+    TextView reservationParkDetailAddressContentTxt;
+    TextView reservationSharerNameContentTxt;
+    TextView reservationSharerPhoneContentTxt;
+    TextView reservationSharerEmailContentTxt;
+    TextView reservationSharerRelationContentTxt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reservation_park);
 
+        reservationParkAddressContentTxt = findViewById(R.id.reservationParkAddressContentTxt);
+        reservationParkDetailAddressContentTxt = findViewById(R.id.reservationParkDetailAddressContentTxt);
+        reservationSharerNameContentTxt = findViewById(R.id.reservationSharerNameContentTxt);
+        reservationSharerPhoneContentTxt = findViewById(R.id.reservationSharerPhoneContentTxt);
+        reservationSharerEmailContentTxt = findViewById(R.id.reservationSharerEmailContentTxt);
+        reservationSharerRelationContentTxt = findViewById(R.id.reservationSharerRelationContentTxt);
+
         Intent inIntent = getIntent();
         reservationFirestoreDocumentId = inIntent.getStringExtra("fireStoreDocumentId");
 
         FirestoreDatabase fd = new FirestoreDatabase();
-
         fd. loadShareParkInfo(reservationFirestoreDocumentId, new OnFirestoreDataLoadedListener() {
             @Override
             public void onDataLoaded(Object data) {
@@ -30,12 +48,43 @@ public class ReservationParkActivity extends AppCompatActivity {
                 for (String key : hm.keySet()) {
                     Object value = hm.get(key);
                     Log.d(TAG, "Key: " + key + ", Value: " + value);
+
+                    TMapData tMapData = new TMapData();
+                    tMapData.reverseGeocoding((Double) hm.get("lat"), (Double) hm.get("lon"), "A02", new TMapData.reverseGeocodingListenerCallback() {
+                        @Override
+                        public void onReverseGeocoding(TMapAddressInfo tMapAddressInfo) {
+                            reservationParkAddressContentTxt.setText(tMapAddressInfo.strFullAddress);
+                        }
+                    });
+
+                    reservationParkDetailAddressContentTxt.setText((String) hm.get("parkDetailAddress"));
+                    reservationSharerNameContentTxt.setText((String) hm.get("ownerName"));
+
+                    String originalPhoneString = (String) hm.get("ownerPhone");
+                    String newPhoneString = "";
+                    if (originalPhoneString != null) {
+                        if (originalPhoneString.length() == 10) {
+                            newPhoneString = originalPhoneString.substring(0, 3) + "-" + originalPhoneString.substring(3, 6) + "-" + originalPhoneString.substring(6);
+                        }
+                        else if (originalPhoneString.length() == 11) {
+                            newPhoneString = originalPhoneString.substring(0, 3) + "-" + originalPhoneString.substring(3, 7) + "-" + originalPhoneString.substring(7);
+                        }
+                        else {
+                            newPhoneString = originalPhoneString;
+                        }
+                    }
+                    reservationSharerPhoneContentTxt.setText(newPhoneString);
+                    reservationSharerEmailContentTxt.setText((String) hm.get("ownerPhone"));
+                    reservationSharerRelationContentTxt.setText((String) hm.get("ownerParkingRelation"));
                 }
             }
 
             @Override
             public void onDataLoadError(String errorMessage) {
                 Log.d(TAG, errorMessage);
+                Intent intent = getIntent();
+                finish();
+                startActivity(intent);
             }
         });
     }
