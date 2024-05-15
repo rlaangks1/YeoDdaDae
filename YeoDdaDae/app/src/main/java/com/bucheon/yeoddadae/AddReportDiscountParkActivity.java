c44c8682c7250dfe02284f65ec704ce1ea650d4e
rlaangks1 <rlaangks1@naver.com>
2024-05-15 16:56:08 +0900

    할인/무료주차장 기능 구현 진행 중

---
+++ YeoDdaDae/app/src/main/java/com/bucheon/yeoddadae/AddReportDiscountParkActivity.java
@@ -0,0 +1,13 @@
+package com.bucheon.yeoddadae;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddReportDiscountParkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_report_discount_park);
+    }
+}
