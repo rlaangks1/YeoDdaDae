554280509c080b0d86b18776403d0dd11732539c
cys-20 <cys20010711@gmail.com>
2024-06-03 20:52:32 +0900

    admin_main 임시 화면 생성 (측면바 activity_main코드 옮겨놓음)

--- YeoDdaDae/app/src/main/java/com/bucheon/yeoddadae/AdminMainActivity.java
+++ YeoDdaDae/app/src/main/java/com/bucheon/yeoddadae/AdminMainActivity.java
@@ -2,19 +2,30 @@ package com.bucheon.yeoddadae;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class AdminMainActivity extends AppCompatActivity {
 
     String loginId;
 
-    Button toApproveShareParkBtn;
-    Button toApproveReportBtn;
-    Button toStatisticsBtn;
-    Button adminLogoutBtn;
+    ImageButton toApproveShareParkBtn;
+    ImageButton toApproveReportBtn;
+    ImageButton toStatisticsBtn;
+    ImageButton adminLogoutBtn;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    ImageButton menubarBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +61,34 @@ public class AdminMainActivity extends AppCompatActivity {
 
             }
         });
+
+        menubarBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.END);
+            }
+        });
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.nav_logout) {
+                    loginId = null;
+                    Intent logoutIntent = new Intent(getApplicationContext(), StartActivity.class);
+                    startActivity(logoutIntent);
+                    finish();
+                }
+                return false;
+            }
+        });
+
+        // 네비게이션뷰 아이디 표시
+        View headerView = navigationView.getHeaderView(0);
+        TextView navHeaderUsername = headerView.findViewById(R.id.nowIdTxt);
+        if (loginId != null) {
+            navHeaderUsername.setText(loginId);
+        }
 
         adminLogoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
